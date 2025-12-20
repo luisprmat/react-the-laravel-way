4f0d3b36672384926e353084d155cca312e09bbf
Luis Parrado <luisprmat@gmail.com>
2025-12-20 13:03:10 -0500

    Toast Notifications

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -27,6 +27,9 @@ export interface SharedData {
     quote: { message: string; author: string };
     auth: Auth;
     sidebarOpen: boolean;
+    flash: {
+        success?: string;
+    };
     [key: string]: unknown;
 }
 
